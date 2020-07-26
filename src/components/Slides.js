b5e08e1424a846cde61dc631fc595318e124df85
HassenH1 <hasansaid51@gmail.com>
2020-07-26 13:20:10 -0700

    done.

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -11,7 +11,7 @@ function Slides() {
   return (
     <div style={{ paddingBottom: "5rem", background: "#ffffff" }}>
       <AutoplaySlider
-        play={false}
+        play={true}
         cancelOnInteraction={false} // should stop playing on user interaction
         interval={6000}
       >
@@ -19,7 +19,7 @@ function Slides() {
           style={{ background: "black", color: "white", textAlign: "center" }}
         >
           <div style={{ margin: "0 auto", fontSize: "36px" }}>
-            <h1>we are driven.</h1>
+            <h1 style={{ margin: "0" }}>we are driven.</h1>
           </div>
           <div style={{ padding: "0 32rem", fontSize: "17px" }}>
             <p>
@@ -28,12 +28,105 @@ function Slides() {
             </p>
           </div>
           <div>
-            <Button color="primary">LEARN MORE ABOUT US</Button>
+            <Button
+              variant="contained"
+              // fullWidth={true}
+              style={{
+                fontSize: "18px",
+                background: "#93CB56",
+                color: "white",
+                borderRadius: "0",
+              }}
+            >
+              <span style={{ padding: "0 1rem" }}>LEARN MORE ABOUT US</span>
+            </Button>
           </div>
         </div>
-        <div style={{ background: "white" }}>2</div>
-        <div style={{ background: "lightgreen" }}>3</div>
-        <div style={{ background: "#00DDDD" }}>4</div>
+        <div
+          style={{ background: "white", color: "black", textAlign: "center" }}
+        >
+          <div style={{ margin: "0 auto", fontSize: "36px" }}>
+            <h1 style={{ margin: "0" }}>we are driven.</h1>
+          </div>
+          <div style={{ padding: "0 32rem", fontSize: "17px" }}>
+            <p>
+              Driven is the largest online cannabis retailer in the state of
+              California, covering 92% and growing.
+            </p>
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              // fullWidth={true}
+              style={{
+                fontSize: "18px",
+                background: "#93CB56",
+                color: "white",
+                borderRadius: "0",
+              }}
+            >
+              <span style={{ padding: "0 1rem" }}>LEARN MORE ABOUT US</span>
+            </Button>
+          </div>
+        </div>
+        <div
+          style={{
+            background: "lightgreen",
+            color: "black",
+            textAlign: "center",
+          }}
+        >
+          <div style={{ margin: "0 auto", fontSize: "36px" }}>
+            <h1 style={{ margin: "0" }}>we are driven.</h1>
+          </div>
+          <div style={{ padding: "0 32rem", fontSize: "17px" }}>
+            <p>
+              Driven is the largest online cannabis retailer in the state of
+              California, covering 92% and growing.
+            </p>
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              // fullWidth={true}
+              style={{
+                fontSize: "18px",
+                background: "#93CB56",
+                color: "white",
+                borderRadius: "0",
+              }}
+            >
+              <span style={{ padding: "0 1rem" }}>LEARN MORE ABOUT US</span>
+            </Button>
+          </div>
+        </div>
+        <div
+          style={{ background: "#00DDDD", color: "black", textAlign: "center" }}
+        >
+          <div style={{ margin: "0 auto", fontSize: "36px" }}>
+            <h1 style={{ margin: "0" }}>we are driven.</h1>
+          </div>
+          <div style={{ padding: "0 32rem", fontSize: "17px" }}>
+            <p>
+              Driven is the largest online cannabis retailer in the state of
+              California, covering 92% and growing.
+            </p>
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              // fullWidth={true}
+              style={{
+                fontSize: "18px",
+                background: "#93CB56",
+                color: "white",
+                borderRadius: "0",
+              }}
+            >
+              <span style={{ padding: "0 1rem" }}>LEARN MORE ABOUT US</span>
+            </Button>
+          </div>
+        </div>
       </AutoplaySlider>
     </div>
   );
