628115e57164a4362f4d818001e19f067af4598b
HassenH1 <hasansaid51@gmail.com>
2020-07-26 12:35:20 -0700

    rfce

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,44 +1,7 @@
-import React, { Component } from "react";
-import Swiper from "swiper";
-// import "swiper/css/swiper.min.css";
+import React from "react";
 
-export default class Slides extends Component {
-  componentDidMount() {
-    this.swiper = new Swiper(".swiper-container", {
-      cssMode: true,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      mousewheel: true,
-      keyboard: true,
-    });
-  }
-
-  render() {
-    return (
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">Slide 1</div>
-          <div className="swiper-slide">Slide 2</div>
-          <div className="swiper-slide">Slide 3</div>
-          <div className="swiper-slide">Slide 4</div>
-          <div className="swiper-slide">Slide 5</div>
-          <div className="swiper-slide">Slide 6</div>
-          <div className="swiper-slide">Slide 7</div>
-          <div className="swiper-slide">Slide 8</div>
-          <div className="swiper-slide">Slide 9</div>
-          <div className="swiper-slide">Slide 10</div>
-        </div>
-        {/* <!-- Add Arrows --> */}
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-        {/* <!-- Add Pagination --> */}
-        <div className="swiper-pagination"></div>
-      </div>
-    );
-  }
+function Slides() {
+  return <div></div>;
 }
+
+export default Slides;
